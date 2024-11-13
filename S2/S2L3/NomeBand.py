94c8fc357d32b32780cdbab24c9a69f35bbb69f5
Francesco110300 <francescodiba@hotmail.i>
2024-11-13 14:53:18 +0100

    Up

---
+++ S2/S2L3/NomeBand.py
@@ -0,0 +1,4 @@
+citta = input("Digita il nome della tua citta': ")
+animale = input("Digita il nome del tuo animale domestico: ")
+nome_band = citta + animale 
+print(f"Il nome della band e': {nome_band}")
